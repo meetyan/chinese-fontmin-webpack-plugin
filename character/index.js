8586f22384e91de34a63cc05a8ac5f51a2d333bb
Jiajun Yan <domo36k@gmail.com>
2022-08-15 16:23:53 +0800

    feat: Added characters as font resources

---
+++ character/index.js
@@ -0,0 +1,15 @@
+const han1000 = require('./1000')
+const han2500 = require('./2500')
+const han7000 = require('./7000')
+const alphabet = require('./alphabet')
+const number = require('./number')
+const punctuation = require('./punctuation')
+
+module.exports = {
+  han1000,
+  han2500,
+  han7000,
+  alphabet,
+  number,
+  punctuation,
+}
