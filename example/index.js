ec8001a838b633918b840dcb005bb38db2183719
Jiajun Yan <domo36k@gmail.com>
2022-08-15 14:04:01 +0800

    feat: Initialized a react example

--- example/index.js
+++
@@ -1,7 +0,0 @@
-const add = (a, b) => {
-  return a + b
-}
-
-alert(`add(1, 2): ${add(1, 2)}`)
-
-console.log('hello1')
--- example/webpack.config.js
+++ example/webpack.config.js
@@ -1,21 +1,42 @@
-const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const path = require('path')
 
-module.exports = {
-  mode: 'development',
-  entry: {
-    main: path.join(__dirname, './index.js'),
-  },
-  output: {
-    filename: 'main.[hash:8].js',
-    path: path.join(__dirname, '../dist'),
-  },
-  plugins: [
-    // 将 output 的 main.[hash:8].js 打包后插入到 index.html
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, './index.html'),
-    }),
-    new CleanWebpackPlugin(), // 每次打包后，清空残留文件
-  ],
+module.exports = (env, argv) => {
+  console.log('webpack config argv =>', argv)
+  const DEV = argv.mode === 'development'
+
+  return {
+    mode: DEV ? 'development' : 'production',
+    entry: './src/index.js',
+    output: {
+      filename: 'bundle.[hash].js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+      }),
+    ],
+    resolve: {
+      modules: [__dirname, 'src', 'node_modules'],
+      extensions: ['*', '.js', '.jsx', '.tsx', '.ts'],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          loader: require.resolve('babel-loader'),
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.png|svg|jpg|gif$/,
+          use: ['file-loader'],
+        },
+      ],
+    },
+  }
 }
