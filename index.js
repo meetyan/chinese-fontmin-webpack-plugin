8a84445ecc6f282aaef7de25ccf36acd5c5cf78c
Jiajun Yan <domo36k@gmail.com>
2022-08-15 17:36:47 +0800

    feat: Added logic to the main file

--- index.js
+++ index.js
@@ -1,10 +1,72 @@
 const Fontmin = require('fontmin')
 const {RawSource} = require('webpack-sources')
+const isPlainObject = require('lodash.isplainobject')
+
+const glyphFiles = require('./glyph')
 
 const pluginName = 'ChineseFontminWebpackPlugin'
 
 class ChineseFontminWebpackPlugin {
-  constructor() {}
+  constructor(glyph = {}) {
+    if (!isPlainObject(glyph)) {
+      const errMsg =
+        'Invalid glyph. Make sure a plain object is passed as a parameter when instantiating.'
+      throw new Error(errMsg)
+    }
+
+    if (glyph.custom && !isPlainObject(glyph.custom)) {
+      const errMsg = 'Invalid custom glyph. Make sure a plain object is passed as a parameter.'
+      throw new Error(errMsg)
+    }
+
+    this.glyph = {
+      han2500: true,
+      han1000: true,
+      han7000: false,
+      alphabet: true,
+      number: true,
+      punctuation: true,
+      custom: {
+        text: '',
+        replace: false,
+      },
+      ...glyph,
+    }
+
+    this.glyphText = ''
+
+    if (this.glyph.custom?.replace) {
+      this.glyphText = this.glyph.custom.text
+      return
+    }
+
+    if (this.glyph.han2500) {
+      this.glyphText = this.glyphText + glyphFiles.han2500
+    }
+
+    if (this.glyph.han1000) {
+      this.glyphText = this.glyphText + glyphFiles.han1000
+    }
+
+    // 因为 7000 字已包含上述的 2500 + 1000，无需再叠加
+    if (this.glyph.han7000) {
+      this.glyphText = glyphFiles.han7000
+    }
+
+    if (this.glyph.alphabet) {
+      this.glyphText = this.glyphText + glyphFiles.alphabet
+    }
+
+    if (this.glyph.number) {
+      this.glyphText = this.glyphText + glyphFiles.number
+    }
+
+    if (this.glyph.punctuation) {
+      this.glyphText = this.glyphText + glyphFiles.punctuation
+    }
+
+    this.glyphText = this.glyphText + (this.glyph.custom?.text || '')
+  }
 
   apply(compiler) {
     /**
@@ -26,26 +88,28 @@ class ChineseFontminWebpackPlugin {
         assets => {
           const fontExtensions = ['.ttf', '.otf']
 
+          // 找到字体文件，提取、打包及替换
           Object.entries(assets).forEach(([pathname, source]) => {
             const fileExtension = `.${pathname.split('.').pop()}`
 
-            if (fontExtensions.includes(fileExtension)) {
-              new Fontmin()
-                .src(source.source())
-                .use(
-                  Fontmin.glyph({
-                    text: '天地玄黄 宇宙洪荒',
-                  })
-                )
-                .run((error, files) => {
-                  if (error) {
-                    const errMsg = 'An error occurred when compressing font with Fontmin.'
-                    return console.error(errMsg, error)
-                  }
+            if (!fontExtensions.includes(fileExtension)) return
 
-                  assets[pathname] = new RawSource(files[0].contents)
+            new Fontmin()
+              .src(source.source())
+              .use(
+                Fontmin.glyph({
+                  text: this.glyphText,
                 })
-            }
+              )
+              .run((error, files) => {
+                if (error) {
+                  console.error('An error occurred when compressing font with Fontmin.')
+                  throw new Error(error)
+                }
+
+                // 打包生成新的字体文件后，替换原有的文件
+                assets[pathname] = new RawSource(files[0].contents)
+              })
           })
         }
       )
@@ -53,4 +117,7 @@ class ChineseFontminWebpackPlugin {
   }
 }
 
-module.exports = ChineseFontminWebpackPlugin
+module.exports = {
+  ChineseFontminWebpackPlugin,
+  glyph: glyphFiles,
+}
